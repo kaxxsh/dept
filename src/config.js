fbdd35f16e3e49c97bc7f764ea3177d83acc358f
kaxxsh <kameshwaran2002@gmail.com>
2023-10-18 20:29:31 +0530

    bug

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NEXT_PUBLIC_NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://cheerful-phoenix-f5d34f.netlify.app";
+    : "https://cheerful-phoenix-f5d34f.netlify.app/";
