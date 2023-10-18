6d71a5fd61b05a730b80ec16e64db817d8632f97
kaxxsh <kameshwaran2002@gmail.com>
2023-10-18 21:42:34 +0530

    bug 4

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,7 +1,7 @@
-import styles from "@/styles/admin.module.scss";
+// import styles from "@/styles/admin.module.scss";
 
-const Page = () => {
-  return <div className={styles.test}>Hello</div>;
-};
+// const Page = () => {
+//   return <div className={styles.test}>Hello</div>;
+// };
 
-export default Page;
+// export default Page;
