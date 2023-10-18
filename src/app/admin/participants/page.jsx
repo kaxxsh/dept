db1a6014215ef21d4dd6ef9e8087a0f4b9597948
kaxxsh <kameshwaran2002@gmail.com>
2023-10-18 21:22:18 +0530

    bug

--- src/app/admin/participants/page.jsx
+++ src/app/admin/participants/page.jsx
@@ -2,12 +2,15 @@ import { BASE_URL } from "@/config";
 import styles from "@/styles/admin.module.scss";
 import Link from "next/link";
 
-const Participants = async () => {
-  const { data } = await fetch(BASE_URL + "/api/registration", {
-    cache: "no-store",
-    credentials: "include",
-    method: "GET",
-  }).then((res) => res.json());
+const Participants = ({ data, error }) => {
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  if (!data) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className={styles.products}>
       <div
@@ -17,7 +20,7 @@ const Participants = async () => {
         }}
       >
         Participant
-        <Link className={styles.add} href="/admin/home/add">
+        <Link className={styles.add} href="/admin/participants/add">
           ADD Participant
         </Link>
       </div>
@@ -31,30 +34,58 @@ const Participants = async () => {
             </tr>
           </thead>
           <tbody>
-            {data.map((item) => {
-              return (
-                <tr key={item._id} style={{ width: "100%" }}>
-                  <td>
-                    <img
-                      src="/profile.png"
-                      alt="prooduct image"
-                      style={{
-                        width: "40px",
-                        height: "40px",
-                        objectFit: "cover",
-                      }}
-                    />
-                  </td>
-                  <td>{item.team}</td>
-                  <td></td>
-                </tr>
-              );
-            })}
+            {data.map((item) => (
+              <tr key={item._id} style={{ width: "100%" }}>
+                <td>
+                  <img
+                    src="/profile.png"
+                    alt="profile image"
+                    style={{
+                      width: "40px",
+                      height: "40px",
+                      objectFit: "cover",
+                    }}
+                  />
+                </td>
+                <td>{item.team}</td>
+                <td></td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
     </div>
   );
 };
+
+export async function getServerSideProps() {
+  try {
+    const response = await fetch(BASE_URL + "/api/registration", {
+      cache: "no-store",
+      credentials: "include",
+      method: "GET",
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    const data = await response.json();
+
+    return {
+      props: {
+        data,
+        error: null,
+      },
+    };
+  } catch (error) {
+    return {
+      props: {
+        data: null,
+        error: error,
+      },
+    };
+  }
+}
 
 export default Participants;
