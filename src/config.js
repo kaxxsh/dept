146689f13612b7c779428c6c645f46ac2870a737
kaxxsh <kameshwaran2002@gmail.com>
2023-10-18 22:02:59 +0530

    config fix

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NEXT_PUBLIC_NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://dept-dm3k79mpf-kaxxsh.vercel.app/";
+    : "https://dept-pearl.vercel.app/";
